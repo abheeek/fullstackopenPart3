35893b93a1af17a064e302aacc67a39be0a9355f
Abheek Dagar <dagarabheek@gmail.com>
2020-11-27 14:24:42 +0530

    Added Update functionality. updated all routes to work with MongoDB

--- index.js
+++ index.js
@@ -13,30 +13,6 @@ app.use(express.static('build'))
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  }
-]
-
 app.get('/api/persons', (req, res) => {
   Person.find({})
     .then(persons => {
@@ -48,12 +24,16 @@ app.get('/api/persons', (req, res) => {
     })
 })
 
-app.get('/info', (req, res) => {
+app.get('/info', (req, res, next) => {
   const date = new Date()
-  res.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${date}</p>
-    `)
+  Person.find({})
+    .then(persons => {
+      res.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>
+      `)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
@@ -84,7 +64,7 @@ app.post('/api/persons', (req, res) => {
   const body = req.body
 
   if (body.name === undefined || body.number === undefined) {
-    return res.status(400).json({ error: 'content missing' })
+    return res.status(400).json({ error: 'name/number missing' })
   }
   const person = new Person({
     name: body.name,
@@ -98,6 +78,25 @@ app.post('/api/persons', (req, res) => {
       console.log(error)
       res.status(500).end()
     })
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  if (body.name === undefined || body.number === undefined) {
+    return res.status(400).json({ error: 'name/number missing'})
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
